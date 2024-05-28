564c175b464414b259a5b3b44a38a58064c2ec50
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-28 00:00:21 -0400

    Set up PhotoListItem to take props from App.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,17 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const { id, location: { city, country }, imageSource, username, profile } = props;
+  
+  return (
+    <div className="photo-list-item">
+      <img src={imageSource} alt={`${username}'s photo`} />
+      <img src={profile} alt={`${username}'s profile`} />
+      <p>{username}</p>
+      <p>{city}, {country}</p>
+    </div>
+  )
 };
 
 export default PhotoListItem;
