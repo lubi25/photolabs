1dc31df4873a739710914f86dc99cb5a8a71d17e
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-29 18:49:40 -0400

    Edit props to take in values from sampleDataForPhotos

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,18 +4,18 @@ import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
-  const { id, location: { city, country }, imageSource, username, profile } = props.photo;
+  const { id, location: { city, country }, urls: { regular }, user: { username, name, profile } } = props.photo;
   
   return (
     <div className="photo-list__item">
       <PhotoFavButton />
       
-      <img src={imageSource} alt={`${username}'s photo`} className="photo-list__image" />
+      <img src={regular} alt={`${username}'s photo`} className="photo-list__image" />
       
       <div className="photo-list__user-details">
         <img src={profile} alt={`${username}'s profile`}  className="photo-list__user-profile" />
         <section className="photo-list__user-info">
-          <div>{username}</div>
+          <div>{name}</div>
           <div className="photo-list__user-location">{city}, {country}</div>
         </section>
       </div>
