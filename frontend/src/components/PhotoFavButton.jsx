67a87c76a082eaecfdab3fa8e642dea70a540a3b
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-29 18:16:51 -0400

    Add initial functionality to change state when clicked

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,15 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [favorite, setFavorite] = useState("inactive");
+  const handleClick = () => setFavorite(favorite === "inactive" ? "active" : "inactive")
+  
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <button onClick={handleClick} className='FavIcon'>
+          <FavIcon selected={favorite === "active"} />
+        </button>
       </div>
     </div>
   );
