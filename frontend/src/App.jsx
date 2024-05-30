eadaf1512908b98f35903aaefc94ecf11ce74d4c
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-29 23:29:04 -0400

    Import mock data and pass on to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import HomeRoute from './components/HomeRoute';
+import photos from './mocks/photos.js';
+import topics from './mocks/topics';
 
 const App = () => {
+  const [photoData, setPhotoData] = useState(photos);
+  const [topicData, setTopicData] = useState(topics);
+
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute photoData={photoData} topicData={topicData} />
     </div>
   );
 };
