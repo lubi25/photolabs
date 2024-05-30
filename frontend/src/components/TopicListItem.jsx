4f53f7103c22137d17d51fe2724a97cbeb2b5933
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-29 20:02:28 -0400

    Create topics component

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,16 +2,18 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
+// const sampleDataForTopicListItem = {
+//   id: "1",
+//   slug: "topic-1",
+//   label: "Nature",
+// };
 
-const TopicListItem = () => {
+const TopicListItem = (props) => {
+  const { id, slug, title } = props.topic;
+
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+      <span>{title}</span>
     </div>
   );
 };
