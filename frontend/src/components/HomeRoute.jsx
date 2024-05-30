27e0bfb8674a45bfc581eef905b1ad3b32e34111
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-29 23:44:52 -0400

    Pass props instead of static data

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -3,11 +3,11 @@ import "../styles/HomeRoute.scss";
 import PhotoList from './PhotoList';
 import TopNavigation from './TopNavigationBar';
 
-const HomeRoute = ({photoData, topicData}) => {
+const HomeRoute = (props) => {
   return (
     <div className="HomeRoute">
-      <TopNavigation topicData={topicData} />
-      <PhotoList photoData={photoData} />
+      <TopNavigation topicData={props.topicData} />
+      <PhotoList photoData={props.photoData} />
     </div>
   );
 };
