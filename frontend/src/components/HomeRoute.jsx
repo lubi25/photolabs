ebe7ebfe01366969bb79757458305ec8aafa76c1
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-30 00:03:43 -0400

    Refactor favorites component

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/HomeRoute.scss";
 import PhotoList from './PhotoList';
 import TopNavigation from './TopNavigationBar';
 
 const HomeRoute = (props) => {
+  const [favorites, setFavorites] = useState([]);
+
+  const toggleFavorite = (photoId) => {
+    setFavorites(prevFavorites =>
+      prevFavorites.includes(photoId)
+        ? prevFavorites.filter(id => id !== photoId)
+        : [...prevFavorites, photoId]
+    );
+  };
+
   return (
     <div className="HomeRoute">
       <TopNavigation topicData={props.topicData} />
-      <PhotoList photoData={props.photoData} />
+      <PhotoList 
+        photoData={props.photoData} 
+        favorites={favorites}
+        onToggleFavorites={toggleFavorite} />
     </div>
   );
 };
