683e907b8eeff82838318d2a33710f4ad139cb51
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-29 22:59:12 -0400

    Move topic list into top navigation component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
-// import PhotoListItem from './components/PhotoListItem';
-import PhotoList from './components/PhotoList';
 import './App.scss';
-import TopicList from 'components/TopicList';
-
-// const photos = new Array(3).fill(null).map(() => ({
-//     id: "1",
-//     location: {
-//       city: "Montreal",
-//       country: "Canada",
-//     },
-//     imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//     username: "Joe Example",
-//     profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-//   }
-// ))
+import PhotoList from './components/PhotoList';
+import TopNavigation from 'components/TopNavigationBar';
 
 const App = () => {
   return (
     <div className="App">
-      {/* {photos.map((photo, index) => (
-        <PhotoListItem key={index} photo={photo} />
-      ))} */}
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
