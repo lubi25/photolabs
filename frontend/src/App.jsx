0772642d2b3221c323cab852bc75a4a4258526b1
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-29 23:09:09 -0400

    Add HomeRoute component to display index page contents

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
-import PhotoList from './components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from './components/HomeRoute';
 
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
