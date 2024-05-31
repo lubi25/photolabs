e84966d7129bbe3c9941a6d772c922fae38b1c0f
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-30 21:42:32 -0400

    Add function to handle modal open and pass down as props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import HomeRoute from './components/HomeRoute';
 import photos from './mocks/photos.js';
 import topics from './mocks/topics';
-import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import PhotoDetailsModal from './components/PhotoDetailsModal';
 
 const App = () => {
   const [photoData, setPhotoData] = useState(photos);
   const [topicData, setTopicData] = useState(topics);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
 
   return (
     <div className="App">
-      <HomeRoute photoData={photoData} topicData={topicData} />
-      <PhotoDetailsModal />
+      <HomeRoute 
+        photoData={photoData} 
+        topicData={topicData} 
+        openModal={handleOpenModal} 
+      />
+      {openModal && <PhotoDetailsModal />}
     </div>
   );
 };
