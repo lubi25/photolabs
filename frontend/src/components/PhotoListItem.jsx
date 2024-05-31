74ab12818aa3d4be702202c0871ca063a8bf591d
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-31 03:42:27 -0400

    Move onClick modal open functionality to the img instead of the whole div

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,14 +7,14 @@ const PhotoListItem = ({ photo, toggleFavorite, isFavorite, openModal }) => {
   const { id, location: { city, country }, urls: { regular }, user: { username, name, profile } } = photo;
   
   return (
-    <div className="photo-list__item" onClick={() => openModal(photo)}>
+    <div className="photo-list__item">
       <PhotoFavButton
         photoId={id}
         isFavorite={isFavorite} // Use the isFavorite prop directly
         toggleFavorite={() => toggleFavorite(id)} // Ensure you're calling toggleFavorite correctly
       />
       
-      <img src={regular} alt={`${username}'s photo`} className="photo-list__image" />
+      <img src={regular} alt={`${username}'s photo`} className="photo-list__image"  onClick={() => openModal(photo)}/>
       
       <div className="photo-list__user-details">
         <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
