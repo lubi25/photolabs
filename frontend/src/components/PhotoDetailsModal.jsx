a449cd4bd73963440898d594c7334525a4dfe009
Lubi Islam <lubaba.islam91@gmail.com>
2024-06-01 19:33:20 -0400

    Replace extraneous code by passing PhotoListItem component

--- frontend/src/components/PhotoDetailsModal.jsx
+++ frontend/src/components/PhotoDetailsModal.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
-import PhotoFavButton from './PhotoFavButton';
 import PhotoList from './PhotoList';
+import PhotoListItem from './PhotoListItem';
 
-const PhotoDetailsModal = ({ selectedPhoto, toggleFavorite, isFavorite, onClose }) => {
+const PhotoDetailsModal = ({ favorites, selectedPhoto, toggleFavorite, onClose }) => {
   const { id, location: { city, country }, urls: { regular }, user: { username, name, profile }, similar_photos } = selectedPhoto;
   
-  const similarPhotosEntries = Object.entries(similar_photos || {});
+  const similarPhotosEntries = Object.values(similar_photos || {});
+
+  const isFavorite = favorites.includes(id);
+  console.log(isFavorite);
 
   return (
     <div className="photo-details-modal">
@@ -16,7 +19,18 @@ const PhotoDetailsModal = ({ selectedPhoto, toggleFavorite, isFavorite, onClose 
         <img src={closeSymbol} alt="close symbol" />
       </button>
 
-      <article className='photo-details-modal__images'>
+    <div className="photo-details-modal__images">
+      <article className='photo-details-modal__image'>
+        <PhotoListItem 
+          photo={selectedPhoto} 
+          toggleFavorite={toggleFavorite} 
+          isFavorite={isFavorite} 
+          openModal={() => {}} 
+        />
+      </article>
+
+
+      {/* <article className='photo-details-modal__images'>
         <PhotoFavButton
           photoId={id}
           isFavorite={isFavorite}
@@ -32,36 +46,12 @@ const PhotoDetailsModal = ({ selectedPhoto, toggleFavorite, isFavorite, onClose 
             <div className="photo-details-modal__photographer-location">{city}, {country}</div>
           </section>
         </div>
-      </article>
+      </article> */}
 
       <div className="photo-details-modal__header">Similar Photos</div>
 
-      <article className='photo-details-modal__images'>
-        <div className="photo-details-modal__similar-photos">
-          {similarPhotosEntries.map(([key, photo]) => (
-            <div key={key} className="photo-details-modal__similar-item">
-              <div className="photo-list__item">
-                <PhotoFavButton
-                  photoId={photo.id}
-                  isFavorite={isFavorite}
-                  toggleFavorite={() => toggleFavorite(photo.id)}
-                />
-
-                <img src={photo.urls.regular} alt={`Similar to ${username}`} className="photo-list__image" />
-
-                <div className="photo-list__user-details">
-                  <img src={profile} alt={`${photo.user.username}'s profile`} className="photo-list__user-profile" />
-                  <section className="photo-list__user-info">
-                    <div>{photo.user.name}</div>
-                    <div className="photo-list__user-location">{photo.location.city}, {photo.location.country}</div>
-                  </section>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-
-      </article>
+      <PhotoList photoData={similarPhotosEntries} toggleFavorite={toggleFavorite} favorites={favorites} openModal={() => {}}  />
+    </div>
     </div>
   );
 };
