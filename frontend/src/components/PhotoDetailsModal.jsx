33a38cecf99ec9ee398fe109663a32e6a572cdf8
Lubi Islam <lubaba.islam91@gmail.com>
2024-06-01 20:23:33 -0400

    Lint

--- frontend/src/components/PhotoDetailsModal.jsx
+++ frontend/src/components/PhotoDetailsModal.jsx
@@ -8,10 +8,9 @@ import PhotoListItem from './PhotoListItem';
 const PhotoDetailsModal = ({ favorites, selectedPhoto, toggleFavorite, onClose }) => {
   const { id, location: { city, country }, urls: { regular }, user: { username, name, profile }, similar_photos } = selectedPhoto;
   
-  const similarPhotosEntries = Object.values(similar_photos || {});
+  const similarPhotosEntries = Object.values(similar_photos);
 
   const isFavorite = favorites.includes(id);
-  console.log(isFavorite);
 
   return (
     <div className="photo-details-modal">
@@ -19,39 +18,27 @@ const PhotoDetailsModal = ({ favorites, selectedPhoto, toggleFavorite, onClose }
         <img src={closeSymbol} alt="close symbol" />
       </button>
 
-    <div className="photo-details-modal__images">
-      <article className='photo-details-modal__image'>
-        <PhotoListItem 
-          photo={selectedPhoto} 
-          toggleFavorite={toggleFavorite} 
-          isFavorite={isFavorite} 
-          openModal={() => {}} 
-        />
-      </article>
+      <div className="photo-details-modal__images">
+        <article className='photo-details-modal__image'>
+          <PhotoListItem 
+            photo={selectedPhoto} 
+            toggleFavorite={toggleFavorite} 
+            isFavorite={isFavorite} 
+            openModal={() => {}} 
+          />
+        </article>
 
+        <div className="photo-details-modal__header">Similar Photos</div>
 
-      {/* <article className='photo-details-modal__images'>
-        <PhotoFavButton
-          photoId={id}
-          isFavorite={isFavorite}
-          toggleFavorite={() => toggleFavorite(id)}
-        />
-        
-        <img src={regular} alt={`${username}'s photo`} className="photo-details-modal__image" />
-        
-        <div className="photo-details-modal__photographer-details">
-          <img src={profile} alt={`${username}'s profile`} className="photo-details-modal__photographer-profile" />
-          <section className="photo-details-modal__photographer-info">
-            <div>{name}</div>
-            <div className="photo-details-modal__photographer-location">{city}, {country}</div>
-          </section>
+        <div className="photo-details-modal__similar-photos">
+          <PhotoList 
+            photoData={similarPhotosEntries} 
+            toggleFavorite={toggleFavorite} 
+            favorites={favorites} 
+            openModal={() => {}}  
+          />
         </div>
-      </article> */}
-
-      <div className="photo-details-modal__header">Similar Photos</div>
-
-      <PhotoList photoData={similarPhotosEntries} toggleFavorite={toggleFavorite} favorites={favorites} openModal={() => {}}  />
-    </div>
+      </div>
     </div>
   );
 };
