e470732ee7382c39091ae8e1d92d7aa24f26d780
Lubi Islam <lubaba.islam91@gmail.com>
2024-06-03 23:57:55 -0400

    Use handleTopicClick to fetch topic when selected

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
-  const { id, slug, title } = props.topic;
+  const { id, title } = props.topic;
 
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item" onClick={() => props.handleTopicClick(id)}>
       <span>{title}</span>
     </div>
   );
